765d4f6b92e149ace3d2a0764142555ae26018f8
Alexander <alex12a7x@gmail.com>
2022-07-26 16:40:59 -0300

    database

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,12 +12,15 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+
   _recuperarBancoDeDados() async{
 
     final caminhoBancoDeDados = await getDatabasesPath();
-    final localBancoDeDados = join(caminhoBancoDeDados, "banco.db");
+    //tive um erro com o nome do banco, parece que se vc tentar criar
+    //o banco varias vezes ele da erro, esse codigo precisa ser otimizado
+    final localBancoDeDados = join(caminhoBancoDeDados, "banco1.db");
     
-    var retorno = await openDatabase(
+    var db = await openDatabase(
       localBancoDeDados,
       version: 1,
       onCreate: (db, dbVersaoRecente){
@@ -28,14 +32,46 @@ class _HomeState extends State<Home> {
         db.execute(sql);
       }
     );
-
-    print("aberto: " + retorno.isOpen.toString());
-
+    //print("aberto: " + db.isOpen.toString());
+    return db;
   }
+
+  _salvar() async {
+
+    Database bd = await _recuperarBancoDeDados();
+
+    Map<String, dynamic> dadosusuario ={
+      "nome" : "Alexander",
+      "idade" : 26
+    };
+    int id = await bd.insert("tb_usuarios", dadosusuario);
+    print("Salvo: $id ");
+  }
+
+  _listarUsusarios() async{
+
+    Database bd = await _recuperarBancoDeDados();
+
+    String sql = "SELECT * FROM tb_usuarios";
+    List usuarios = await bd.rawQuery(sql);
+
+    for(var usuario in usuarios){
+      print(
+          "item id: " + usuario['id_usuario'].toString() +
+          " nome: " + usuario['nome'] +
+          " idade: "+ usuario['idade'].toString()
+      );
+    }
+    //print("usuarios: " + usuarios.toString());
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    _recuperarBancoDeDados();
+    //_salvar();
+    _listarUsusarios();
     return Container();
   }
 }
